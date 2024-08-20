590c79c474d27d58b8b495d8d164121efdfd3194
devamanoj <devanathanmanoj00714@gmail.com>
2024-08-20 19:03:53 +0530

    uploading

--- formproject/forms/userprofile/models.py
+++ formproject/forms/userprofile/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class review(models.Model):
+    user_name = models.CharField(max_length=30)
+    rating = models.IntegerField()
+    massage = models.TextField()
+    
--- formproject/forms/userprofile/views.py
+++ formproject/forms/userprofile/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.http import *
 from .forms import details
+# from .models import Review
 # Create your views here.
 def forms(request):
     if request.method == "POST":
         form_data = details(request.POST) # i will get the data from the froms
-        print(form_data)
         if form_data.is_valid(): # i it check it is valid are nt inside the html page it self 
+            # datas = Review(
+            #     user_name = form_data.cleaned_data["user_name_2"],
+            #     review = form_data.cleaned_data["rating"],
+            #     message = form_data.cleaned_data["message"]
+            # )
+            # datas.save()
+            form_data.save()
             return HttpResponseRedirect("feedback")
-    form_data = details() # it will act like the form and we written in the forms.py 
-    return render(request,"forms/forms.html", {"data":form_data})
+    else:
+    
+        
+        form_data = details() # it will act like the form and we written in the forms.py 
+    return render(request,"forms/forms.html", {"form":form_data})
 
 def feedback(request):
     return render(request,"forms/thankyou.html")
